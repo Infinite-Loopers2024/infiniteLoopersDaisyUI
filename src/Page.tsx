215651213064fce429b3f67aaa75cc665fd9184e
defycab <love.bellinder@gmail.com>
2024-09-28 21:56:57 +0200

    making daisyui project, setup and first iteration

---
+++ src/Page.tsx
@@ -0,0 +1,51 @@
+function App() {
+  return (
+    <>
+      <div className="navbar rounded-sm bg-primary text-primary-content justify-center">
+        <p className="text-2xl font-extrabold ">Infinite Loopers</p>
+      </div>
+      <body>
+        <div className="flex">
+          <section className="box avatar">
+            <p className="mx-5">Love</p>
+            <div className="w-24 rounded-full">
+              <img src="https://ca.slack-edge.com/TA01UCHBN-U07J8U0R0DD-24739eb0a367-512" />
+            </div>
+          </section>
+          <section className="box avatar">
+            <p className="mx-5">Ali</p>
+            <div className="w-24 rounded-full">
+              <img src="https://ca.slack-edge.com/TA01UCHBN-U07KQ7DPKH8-de79f32545a9-512" />
+            </div>
+          </section>
+          <section className="box avatar">
+            <p className="mx-5">Adrienn</p>
+            <div className="w-24 rounded-full">
+              <img src="https://ca.slack-edge.com/TA01UCHBN-U07JLJFEQN9-f68cc50108c1-512" />
+            </div>
+          </section>
+          <section className="box avatar">
+            <p className="mx-5">Mårten</p>
+            <div className="w-24 rounded-full">
+              <img src="https://ca.slack-edge.com/TA01UCHBN-U07KC8Q6ZFS-21b199c7680c-512" />
+            </div>
+          </section>
+        </div>
+      </body>
+    </>
+  )
+}
+export default App
+
+// <section className="card bg-base-100 w-96 shadow-xl mt-20 mx-10">
+//   <figure>
+//     <img src="../public/LoveLooper.jpg" alt="Looper" />
+//   </figure>
+//   <div className="cardbody">
+//     <h2 className="card-title">Infinite Looper!</h2>
+//     <p>Love Bellinder</p>
+//     <div className="card-actions justify-end">
+//       <button className="btn btn-primary">Show!</button>
+//     </div>
+//   </div>
+// </section>
